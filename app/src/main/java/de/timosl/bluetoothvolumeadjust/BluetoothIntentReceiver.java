7af4843c9c536bf4124457d72c385f0a096a8a55
Timo Schlömer <timo.schloemer@uni-oldenburg.de>
2016-08-03 21:32:55 +0200

    Fixes #2
    Added a setting for resetting the media volume after a device disconnects.

--- app/src/main/java/de/timosl/bluetoothvolumeadjust/BluetoothIntentReceiver.java
+++ app/src/main/java/de/timosl/bluetoothvolumeadjust/BluetoothIntentReceiver.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.preference.Preference;
 import android.util.Log;
 
 /**
@@ -34,68 +35,127 @@ public class BluetoothIntentReceiver extends BroadcastReceiver {
             return;
         }
 
-        // Only adjust volume if a Bluetooth device is now CONNECTED
+        // Perform the necessary actions when a device is CONNECTING
+        if(state == BluetoothProfile.STATE_CONNECTING) {
+            onDeviceConnecting(context);
+        }
+
+        // Perform the necessary actions when a device is CONNECTED
         if(state == BluetoothProfile.STATE_CONNECTED) {
-            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            onDeviceConnected(context, device);
+        }
 
-            // If we're already playing music on the Bluetooth device, we can adjust the volume right away
-            if(audioManager.isBluetoothA2dpOn() && audioManager.isMusicActive()) {
-                adjustAudio(context,device);
-            }
-
-            // If there is currently no music playing, adjusting the volume of the Music-Channel
-            // will not affect the volume for the bluetooth device. Android handles music volume over
-            // bluetooth differently and will only allow changes when music is actively being played
-            // over bluetooth.
-            else {
-                // Store the time so we can abort after a certain interval
-                long musicWaitBegin = System.currentTimeMillis();
-
-                // If no music is currently playing, a silent music track
-                // will be played
-                if(!audioManager.isMusicActive()) {
-                    Log.i(MainActivity.TAG,"No music is currently being played. Playing a silent track to enable proper volume adjustment");
-
-                    try {
-                        // Create and start a MediaPlayer playing a silent music track
-                        MediaPlayer silencePlayer = MediaPlayer.create(context, R.raw.silence);
-                        silencePlayer.start();
-
-                        // Release the resources of the MediaPlayer after the
-                        // track has been played
-                        silencePlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                            @Override
-                            public void onCompletion(MediaPlayer mp) {
-                                mp.release();
-                            }
-                        });
-                    } catch(Exception exception) {
-                        Log.e(MainActivity.TAG,"There was an error playing the silent track: "+exception);
-                    }
-                }
-
-                // Stay in this loop until music is being played
-                while(!audioManager.isMusicActive()) {
-                    try {
-                        // Don't completely waste CPU cycles
-                        Thread.sleep(100L);
-
-                        // Abort if we waited too long already
-                        if(System.currentTimeMillis() - musicWaitBegin > MUSIC_TIMEOUT) {
-                            Log.w(MainActivity.TAG,"There was no music playing after "+(System.currentTimeMillis() - musicWaitBegin)+"ms, not adjusting volume");
-                            return;
-                        }
-                    } catch (InterruptedException e) {}
-                }
-                Log.d(MainActivity.TAG,"Waited "+(System.currentTimeMillis()-musicWaitBegin)+"ms");
-
-                // If we ended up here, there should be music playing on the Bluetooth device,
-                // so we can finally adjust the volume
-                adjustAudio(context,device);
-            }
+        // Perform the necessary actions when a device is DISCONNECTED
+        if(state == BluetoothProfile.STATE_DISCONNECTED) {
+            onDeviceDisconnected(context);
         }
     }
 
+    /**
+     * Called when a registered device is being connected. (This usually
+     * means the media stream has not yet switched to Bluetooth)
+     * @param context The applications {@link Context}
+     */
+    private void onDeviceConnecting(Context context) {
+        // Get a reference to the audio manager
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        // Store the current media volume so we can reset it later (if needed)
+        int currentMediaVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        Preferences.setLastMediaVolume(context,currentMediaVolume);
+    }
+
+    /**
+     * Called when a registered device is now connected.
+     * @param context The applications {@link Context}
+     * @param device The {@link BluetoothDevice} that has connected
+     */
+    private void onDeviceConnected(Context context, BluetoothDevice device) {
+        // Get a reference to the audio manager
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        // If we're already playing music on the Bluetooth device, we can adjust the volume right away
+        if(audioManager.isBluetoothA2dpOn() && audioManager.isMusicActive()) {
+            adjustAudio(context,device);
+        }
+
+        // If there is currently no music playing, adjusting the volume of the Music-Channel
+        // will not affect the volume for the bluetooth device. Android handles music volume over
+        // bluetooth differently and will only allow changes when music is actively being played
+        // over bluetooth.
+        else {
+            // Store the time so we can abort after a certain interval
+            long musicWaitBegin = System.currentTimeMillis();
+
+            // If no music is currently playing, a silent music track
+            // will be played
+            if(!audioManager.isMusicActive()) {
+                Log.i(MainActivity.TAG,"No music is currently being played. Playing a silent track to enable proper volume adjustment");
+
+                try {
+                    // Create and start a MediaPlayer playing a silent music track
+                    MediaPlayer silencePlayer = MediaPlayer.create(context, R.raw.silence);
+                    silencePlayer.start();
+
+                    // Release the resources of the MediaPlayer after the
+                    // track has been played
+                    silencePlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                        }
+                    });
+                } catch(Exception exception) {
+                    Log.e(MainActivity.TAG,"There was an error playing the silent track: "+exception);
+                }
+            }
+
+            // Stay in this loop until music is being played
+            while(!audioManager.isMusicActive()) {
+                try {
+                    // Don't completely waste CPU cycles
+                    Thread.sleep(100L);
+
+                    // Abort if we waited too long already
+                    if(System.currentTimeMillis() - musicWaitBegin > MUSIC_TIMEOUT) {
+                        Log.w(MainActivity.TAG,"There was no music playing after "+(System.currentTimeMillis() - musicWaitBegin)+"ms, not adjusting volume");
+                        return;
+                    }
+                } catch (InterruptedException e) {}
+            }
+            Log.d(MainActivity.TAG,"Waited "+(System.currentTimeMillis()-musicWaitBegin)+"ms");
+
+            // If we ended up here, there should be music playing on the Bluetooth device,
+            // so we can finally adjust the volume
+            adjustAudio(context,device);
+        }
+    }
+
+    /**
+     * Called when a registered device is now disconnected.
+     * @param context The applications {@link Context}
+     */
+    private void onDeviceDisconnected(Context context) {
+        // Get a reference to the audio manager
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        // Reset the media volume if the user enabled the corresponding setting
+        if(Preferences.getResetVolumeOnDisconnect(context)) {
+            // Check the user preference if the volume indicator should be displayed
+            int showIndicatorFlag = Preferences.getShowIndicatorEnabled(context) ? AudioManager.FLAG_SHOW_UI : 0;
+
+            // Get the previous volume
+            int previousVolume = Preferences.getLastMediaVolume(context);
+
+            // Ensure a previous volume was stored
+            if(previousVolume != -1) {
+                // Set the volume to its previous level
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,previousVolume,showIndicatorFlag);
+
+                Log.d(MainActivity.TAG,"Volume reset to previous value");
+            }
+        }
+    }
 
     /**
      * Adjusts the volume for the currently connected device.
--- app/src/main/java/de/timosl/bluetoothvolumeadjust/Preferences.java
+++ app/src/main/java/de/timosl/bluetoothvolumeadjust/Preferences.java
@@ -1,6 +1,8 @@
 package de.timosl.bluetoothvolumeadjust;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
 /**
@@ -14,13 +16,53 @@ public class Preferences {
     private static final String KEY_SHOW_INDICATOR = "pref_show_indicator";
 
     /**
+     * Key for the volume reset on disconnect setting.
+     */
+    private static final String KEY_RESET_VOLUME_ON_DISCONNECT = "pref_reset_volume_on_disconnect";
+
+    /**
+     * Key for the last media volume setting.
+     */
+    private static final String KEY_LAST_MEDIA_VOLUME = "pref_last_media_volume";
+
+    /**
      * Returns if the systems volume indicator should be shown during volume changes or
      * if it should be hidden.
      * @param context The applications {@link Context}
      * @return Returns 'true' only, if the systems volume indicator should be shown,
-     * 'false' if it should be hidden.
+     * 'false' if it should be hidden
      */
     public static boolean getShowIndicatorEnabled(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_SHOW_INDICATOR,true);
+    }
+
+    /**
+     * Returns if the volume should be set to its previous value after a device disconnects.
+     * @param context The applications {@link Context}
+     * @return Returns 'true' only, if the volume should be reset and 'false' if no action
+     * should be taken after a device disconnects
+     */
+    public static boolean getResetVolumeOnDisconnect(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_RESET_VOLUME_ON_DISCONNECT,false);
+    }
+
+    /**
+     * Sets the last media volume.
+     * @param context The applications {@link Context}
+     * @param volume The volume to store for restoring it later
+     */
+    public static void setLastMediaVolume(Context context, int volume) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putInt(KEY_LAST_MEDIA_VOLUME,volume);
+        editor.commit();
+    }
+
+    /**
+     * Returns the last media volume.
+     * @param context The applications {@link Context}
+     * @return The last media volume before
+     */
+    public static int getLastMediaVolume(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getInt(KEY_LAST_MEDIA_VOLUME,-1);
     }
 }
